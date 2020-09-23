3b5b229e626f5f771b6649e1cd784eb54f7933b3
ArtemKarlov <ar.karlov@gmail.com>
2020-09-23 23:03:57 +0400

    replace circle to recurce

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 "use strict";
 
+//
+// Response: {tickets: [], stop: true}
+//
+// interface Ticket {
+//   // Цена в рублях
+//   price: number
+//   // Код авиакомпании (iata)
+//   carrier: string
+//   // Массив перелётов.
+//   // В тестовом задании это всегда поиск "туда-обратно" значит состоит из двух элементов
+//   segments: [
+//     {
+//       // Код города (iata)
+//       origin: string
+//       // Код города (iata)
+//       destination: string
+//       // Дата и время вылета туда
+//       date: string
+//       // Массив кодов (iata) городов с пересадками
+//       stops: string[]
+//       // Общее время перелёта в минутах
+//       duration: number
+//     },
+//     {
+//       // Код города (iata)
+//       origin: string
+//       // Код города (iata)
+//       destination: string
+//       // Дата и время вылета обратно
+//       date: string
+//       // Массив кодов (iata) городов с пересадками
+//       stops: string[]
+//       // Общее время перелёта в минутах
+//       duration: number
+//     }
+//   ]
+// }
+//
+
 const url = "https://front-test.beta.aviasales.ru";
 
 window.onload = async function () {
   try {
     let searchId = await getSearchId(url);
-    // console.log(searchId);
-    let isRepeatTicketsRequest = true;
-    do {
-      let tickets = await getTickets(url, searchId);
-      console.log(tickets);
-      if (tickets.stop) isRepeatTicketsRequest = false;
-    } while (isRepeatTicketsRequest);
+    console.log(searchId);
+    let tickets = await search(url, searchId);
+    console.log(tickets);
   } catch (error) {
     console.log(error.message);
   }
@@ -36,6 +71,18 @@ async function getTickets(url, params) {
     let json = await response.json();
     return json;
   } else {
-    throw new Error(json.error);
+    console.log("getTickets error");
+    // throw new Error(json.error);
   }
+}
+
+async function search(url, searchId) {
+  const response = await getTickets(url, searchId);
+  const tickets = [];
+  tickets.push(response.tickets);
+  if (!response.stop) {
+    tickets.push(search(url, searchId));
+  }
+
+  return tickets;
 }
