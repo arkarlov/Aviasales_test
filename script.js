91be7fd226e7026da8060d1fd33ae4209880734e
ArtemKarlov <ar.karlov@gmail.com>
2020-09-25 00:05:47 +0400

    fix render route template

--- script.js
+++ script.js
@@ -41,41 +41,126 @@
 //  CDN: //pics.avs.io/99/36/{IATA_CODE_HERE}.png
 //
 
-
-const TestTicket = {
-    // Цена в рублях
-    price: 134000,
-    // Код авиакомпании (iata)
-    carrier: '7J',
-    // Массив перелётов.
-    // В тестовом задании это всегда поиск "туда-обратно" значит состоит из двух элементов
-    segments: [
-      {
-        // Код города (iata)
-        origin: 'KUF',
-        // Код города (iata)
-        destination: 'LHR',
-        // Дата и время вылета туда
-        date: '10:45 – 08:00',
-        // Массив кодов (iata) городов с пересадками
-        stops: ['HKG', 'JNB', 'SVO'],
-        // Общее время перелёта в минутах
-        duration: 1275,
+const testResponce = {
+  tickets: [
+      {   price: 134000,
+          carrier: '7J',
+          segments: [
+            {
+              origin: 'KUF',
+              destination: 'LHR',
+              date: '10:45 – 08:00',
+              stops: ['HKG', 'SVO'],
+              duration: 1275,
+            },
+            {
+              origin: 'LHR',
+              destination: 'KUF',
+              date: '08:00 – 10:45',
+              stops: ['JNB', 'HKG'],
+              duration: 1275,
+            }
+          ]
       },
-      {
-        // Код города (iata)
-        origin: 'LHR',
-        // Код города (iata)
-        destination: 'KUF',
-        // Дата и время вылета обратно
-        date: '08:00 – 10:45',
-        // Массив кодов (iata) городов с пересадками
-        stops: ['JNB', 'HKG'],
-        // Общее время перелёта в минутах
-        duration: 1275,
-      }
-    ]
-  }
+      {   price: 13400,
+          carrier: 'S7',
+          segments: [
+            {
+              origin: 'AHJ',
+              destination: 'JNB',
+              date: '10:45 – 08:00',
+              stops: ['HKG', 'JNB', 'DHF'],
+              duration: 1275,
+            },
+            {
+              origin: 'JNB',
+              destination: 'AHJ',
+              date: '08:00 – 10:45',
+              stops: ['AUE'],
+              duration: 2275,
+            }
+          ]
+      },
+      {   price: 1340,
+          carrier: 'JP',
+          segments: [
+            {
+              origin: 'AGV',
+              destination: 'WAM',
+              date: '10:45 – 08:00',
+              stops: ['HKG'],
+              duration: 5275,
+            },
+            {
+              origin: 'WAM',
+              destination: 'AGV',
+              date: '08:00 – 10:45',
+              stops: ['HKG'],
+              duration: 6275,
+            }
+          ]
+      },
+      {   price: 234000,
+          carrier: 'QU',
+          segments: [
+            {
+              origin: 'BML',
+              destination: 'SDY',
+              date: '10:45 – 08:00',
+              stops: ['HKG', 'JNB', 'SVO'],
+              duration: 275,
+            },
+            {
+              origin: 'SDY',
+              destination: 'BML',
+              date: '08:00 – 10:45',
+              stops: ['JNB', 'HKG', 'KSM'],
+              duration: 175,
+            }
+          ]
+      },
+      {   price: 13000,
+          carrier: 'TK',
+          segments: [
+            {
+              origin: 'KUF',
+              destination: 'LHR',
+              date: '10:45 – 08:00',
+              stops: ['HKG', 'JNB', 'KSM'],
+              duration: 1075,
+            },
+            {
+              origin: 'LHR',
+              destination: 'KUF',
+              date: '08:00 – 10:45',
+              stops: ['JNB', 'HKG'],
+              duration: 875,
+            }
+          ]
+      },
+      {   price: 4000,
+          carrier: 'QR',
+          segments: [
+            {
+              origin: 'JNB',
+              destination: 'HKG',
+              date: '10:45 – 08:00',
+              stops: ['HKG', 'SVO'],
+              duration: 3275,
+            },
+            {
+              origin: 'HKG',
+              destination: 'JNB',
+              date: '08:00 – 10:45',
+              stops: ['JNB'],
+              duration: 1275,
+            }
+          ]
+      },
+  ], 
+  stop: true,
+};
+
 
 
 
@@ -83,7 +168,10 @@ const url = "https://front-test.beta.aviasales.ru";
 
 window.onload = async function () {
   try {
-    renderTicket(TestTicket);
+    renderOneTicket(testResponce.tickets[0]);
+    renderOneTicket(testResponce.tickets[1]);
+    renderOneTicket(testResponce.tickets[2]);
+    renderOneTicket(testResponce.tickets[3]);
     // let searchId = await getSearchId(url);
     // let tickets = await search(url, searchId);
     // console.log(tickets);
@@ -92,6 +180,7 @@ window.onload = async function () {
   }
 };
 
+// отправляем запрос на сервер, получаем saercID
 async function getSearchId(url) {
   let searchUrl = new URL("search", url);
   let response = await fetch(searchUrl);
@@ -103,6 +192,7 @@ async function getSearchId(url) {
   }
 }
 
+// отправляем запрос на сервер, получаем пачку билетов
 async function getTickets(url, params) {
   let ticketsUrl = new URL("tickets", url);
   ticketsUrl.searchParams.append("searchId", params.searchId);
@@ -116,6 +206,7 @@ async function getTickets(url, params) {
   }
 }
 
+// ищем билеты, пока не получим responce.stop == true
 async function search(url, searchId) {
   const response = await getTickets(url, searchId);
   const tickets = [];
@@ -127,16 +218,17 @@ async function search(url, searchId) {
   return tickets;
 }
 
-function renderTicket(ticketData) {
-  const carrierLogoUrl = `//pics.avs.io/99/36/${ticketData.carrier}.png`
-  const flight = ticketData.segments[0];
-  const returnFlite = ticketData.segments[1];
+// отрисовываем один билет, ticketData - объект билета
+function renderOneTicket(ticketData) {
+  const carrierLogoUrl = `//pics.avs.io/99/36/${ticketData.carrier}.png` // получаем logo.png авиакомпании из CDN
 
-  const ticketsSection = document.querySelector('#tickets-section');
   const ticketTemplate = document.querySelector('#ticket-template').content;  
   const ticket = ticketTemplate.cloneNode(true);
   const ticketPrice = ticket.querySelector('.ticket__price');
   const ticketLogo = ticket.querySelector('.ticket__logo');
+
+  ticketPrice.innerHTML = ticketData.price + "&nbsp;&#8381;";
+  ticketLogo.src = carrierLogoUrl;
 
   ticketData.segments.forEach(element => {
     const routeTemplate = ticket.querySelector('#route-template').content;
@@ -147,19 +239,22 @@ function renderTicket(ticketData) {
     const routeSstopsCount = ticketRoute.querySelector('.route-stops-count');
     const routeStops = ticketRoute.querySelector('.route-stops');
 
+    let routeDuration = {};
+    routeDuration.mins = element.duration % 60;
+    routeDuration.hours = (element.duration - routeDuration.mins) / 60;
+
     routeTitle.textContent = `${element.origin} – ${element.destination}`;
     routeTime.textContent = element.date;
-    routeLenght.textContent = element.duration;
+    routeLenght.textContent = `${routeDuration.hours}ч ${routeDuration.mins}м`;
     routeSstopsCount.textContent = element.stops.length;
     routeStops.textContent = element.stops.join(', ');
 
-    ticket.append(ticketRoute);   
+    ticket.children[0].append(ticketRoute);   // добавляем в потомка DocumentFragment
   });
 
-  ticketPrice.innerHTML = ticketData.price + "&nbsp;&#8381;";
-  ticketLogo.src = carrierLogoUrl;
+  document.querySelector('#tickets-section').append(ticket);
+};
 
-  console.log(ticket);
+function renderAllTickets() {
 
-  ticketsSection.append(ticket);
 }
